1154a105f5c040ee9b92349af8b8ecbf4543a180
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-09-17 02:21:30 +0700

    first commit

---
+++ src/services/EventService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_BACKEND_URL,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/auctionitems?_limit=' + perPage + '&_page=' + page)
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get('/auctionitems/' + id)
+  },
+  saveEvent(event) {
+    return apiClient.post('/auctionitems', event)
+  },
+  getEventByKeyword(keyword, perPage, page) {
+    return apiClient.get(
+      'auctionitems?_limit=' + perPage + '&_page=' + page + '&description=' + keyword 
+    )
+  }
+}
